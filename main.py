43f7f9d61dc00193041a6580a6e5ca02e0f4d9be
Stefano P <lordstephen77@localhost.localdomain>
2017-10-16 00:39:51 +0200

    start project

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+* @name        Computer Management Systems
+* @copyright   Stefano Peris (c) 2017
+* @author      Stefano Peris
+* @email       lordstephen77@gmail.com
+* @github      https://github.com/LordStephen77/Computer-MS
+* @license     GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+* @create      lun 16 ott 2017 00:31:27 CEST
+* @update      null
+"""
+
+from tkinter import *
+import random
+import time
+
+root = Tk()
+root.geometry("1600x800+0+0")
+root.title("Computer Management Systems")
+
+root.mainloop()
