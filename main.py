cfec933a9bd9b8f5cf2f97eea876f4378e9b9ec5
Stefano P <lordstephen77@localhost.localdomain>
2017-10-16 01:15:08 +0200

    added frames

--- main.py
+++ main.py
@@ -20,4 +20,18 @@ root = Tk()
 root.geometry("1600x800+0+0")
 root.title("Computer Management Systems")
 
+Tops = Frame(root, width = 1600, height = 50, bg = "powder blue", relief = SUNKEN)
+Tops.pack(side = TOP)
+
+f1 = Frame(root, width = 800, height = 700, bg = "powder blue", relief = SUNKEN)
+f1.pack(side = LEFT)
+
+f2 = Frame(root, width = 300, height = 700, bg = "powder blue", relief = SUNKEN)
+f2.pack(side = RIGHT)
+
+lblInfo = Label(Tops, font = ("arial", 50, "bold"), text = "Computer Management Systems", fg = "Steel Blue", bd = "10", anchor = "w")
+lblInfo.grid(row = 0, column = 0)
+lblInfo = Label(Tops, font = ("arial", 50, "bold"), text = "Computer Management Systems", fg = "Steel Blue", bd = "10", anchor = "w")
+lblInfo.grid(row = 1, column = 0)
+
 root.mainloop()
